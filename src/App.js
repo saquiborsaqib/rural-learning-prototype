c27421b7d2cc2b89c12048bd9bcde1cd10225012
Saquib Zia <szsiddiqui990876@gmail.com>
2025-09-20 21:51:31 +0530

    Fix lockfile for Vercel deployment

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
 import Lessons from "./pages/Lessons";
 import Analytics from "./pages/Analytics";
 import "./i18n/i18n";
@@ -13,11 +9,11 @@ import "./i18n/i18n";
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/lessons" element={<Lessons/>}/>
-        <Route path="/analytics" element={<Analytics/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/lessons" element={<Lessons />} />
+        <Route path="/analytics" element={<Analytics />} />
       </Routes>
     </Router>
   );
